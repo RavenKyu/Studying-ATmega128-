1fd95b818ac2903781c76e014ccb672cfef23dc1
Raven <hong18s@gmail.com>
2012-11-09 02:35:00 +0900

    LED가 이동하는것 처럼 켜지고 꺼진다. * Shift 연산을 통하여 LED가 움직이는 것 처럼 보인다.

--- LED/led.c
+++ LED/led.c
@@ -4,13 +4,23 @@ volatile unsigned delay(int iv);
 
 int main(void)
 {
+    unsigned int val = 0;
+    
     DDRC = 0xff;                /* 출력으로 설정 */
     PORTC = 0xff;               /* 포트 초기화 */
     
     while(1)
     {
-        PORTC = ~PORTC;         /* 단순한 LED 껏다 켰다를 반복 */
+        PORTC = ~(1 << val);    /* shift 연산: 1을 val 만큼 옮기면서 간다. */
+        val = val + 1;
+        
         delay(5);
+
+        if(val > 4)             /* LED 끝까지 왔을 경우 */
+        {
+            val = 0;            /* 다시 처음으로 돌아간다. */
+        }
+        
     }
     
     return 1;
