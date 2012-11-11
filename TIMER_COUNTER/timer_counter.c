b065487372573c7aedc89a524926f353e300d8f6
Raven <hong18s@gmail.com>
2012-11-12 03:23:37 +0900

    외부 인터럽트 * 외부 인터럽트 INT4를 이용하여 LED를 점등 및 소등. * Season 2에서는 IR 수신 센서를 이용

--- TIMER_COUNTER/timer_counter.c
+++ TIMER_COUNTER/timer_counter.c
@@ -6,10 +6,17 @@
 #define TICKS_PER_SEC   1000
 
 volatile unsigned int tic_time;
+volatile char interrupt_flag;
 
 SIGNAL(SIG_OUTPUT_COMPARE0)
 {
     tic_time++;
+}
+
+/* INT4의 외부 인터럽트 함수 설정 */
+SIGNAL(SIG_INTERRUPT4)
+{
+    interrupt_flag = 1;
 }
 
 void delay_ms(unsigned int msec)
@@ -29,17 +36,24 @@ int main(void)
     TCCR0 = (1 << CS02) | (0 << CS01) | (0 << CS00) | (0 << WGM00) | (1 << WGM01); /* 비교 인터럽트 */
     OCR0 = 250;                 /* OCR을 초기화 6을 따로 줄 필요가 없다. */
 
+    EICRB = (0 << ISC41) | (0 << ISC40); /* INT4를 상승엣지 인터럽트 설정 */
+    EIMSK = (1 << INT4);                 /* INT4를 활성화 */
+
     TIMSK = (1 << OCIE0) | (0 << TOIE0);
 
     sei();
 
     while(1)
     {
-        led_status = ~led_status;
+        if(interrupt_flag == 1) /* 인터럽트가 들어왔을 경우. */
+        {
+            led_status = ~led_status;
         
-        PORTC = led_status;
+            PORTC = led_status;
 
-        delay_ms(1000);
+            delay_ms(1000);
+            interrupt_flag = 0;
+        }
     }
 
     return 1;
